c441422cc58a3e7907042927f49669baf37929a3
vigneshwarbiju <vigneshwarbiju@gmail.com>
2026-02-13 23:27:39 +0530

    forloop list

--- forloop list 10num.py
+++ forloop list 10num.py
@@ -1,11 +1,11 @@
-a=[]
+b=[]
 print("Enter 5 numbers:")
 for i in range(5):
     num=int(input (f"Enter number {i+1}: "))
-    a.append(num)
-print(a)
+    b.append(num)
+print(b)
 
 sum=0
-for i in a:
+for i in b:
     sum=sum+i
 print("The sum of the numbers is:",sum)
